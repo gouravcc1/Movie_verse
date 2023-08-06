8e6bcddbfe5d23f30ddc055ce349a6442115d502
Gourav Choudhary <gouravcc1@gmail.com>
2023-08-06 20:28:49 +0530

    use nevigate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,35 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
-import { BsSearch } from 'react-icons/bs';
-import { FaHome } from 'react-icons/fa';
+import { Link , useNavigate} from "react-router-dom";
+import { BsBackspace, BsSearch } from "react-icons/bs";
+import { FaHome } from "react-icons/fa";
 export default function Header() {
-  const [name, setName] = useState('');
-  
+  const [name, setName] = useState("");
+  const navigate = useNavigate()
+
+
   function handleChange(events) {
     setName(events.target.value);
   }
+  useEffect(() => {
+      if (name !== "") {
+        navigate("/Search/" + name);
+      }else{
+        navigate("/");
+
+    }
+  }, [name, navigate]);
   console.log(name);
   return (
     <div className="Header">
-        <div className="homeicondiv">
-      <Link to={"/"} className="link">  
-      <FaHome  className="homeicon"/>
-      </Link>
+      <div className="homeicondiv">
+        <Link to={"/"} className="link">
+          <FaHome className="homeicon" />
+        </Link>
       </div>
+
       <h1 className="demoFont">The Entertainment Commynity</h1>
-    
-        <div className="search">
+      <div className="search">
         <input
           className="search"
           name="name"
@@ -28,11 +38,10 @@ export default function Header() {
           type="text"
           placeholder="Search"
         />
-      <Link to={"/Search/" + name} className="link">
-        <BsSearch className="icon" />
-      </Link>
+        {/* <Link to={"/Search/" + name} className="link">
+        </Link> */}
+        {/* <BsSearch className="icon" /> */}
       </div>
-
     </div>
   );
 }
